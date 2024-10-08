c5ba3522514853b1227a203662caceb0436ec3a3
Thomas Augot <thomas.augot@hotmail.fr>
2024-10-08 11:09:27 +0100

    pwa ok en dev mode

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
   runtimeCaching: [
     // Cache static assets such as JavaScript, CSS, etc.
     {

877912c28a8ba1c9823318dfec676b755c006bce
Thomas Augot <thomas.augot@hotmail.fr>
2024-10-08 10:30:23 +0100

    pwa descargable

--- app/layout.js
+++ app/layout.js
@@ -1,44 +1,28 @@
 import "./globals.css";
-import Head from "next/head";
 import TranslationProvider from "@/components/TranslationProvider";
 import StoreProvider from "@/components/StoreProvider";
+import Head from "next/head";
+
+export const metadata = {
+  title:
+    "Energia Solar Canarias - Instalación de Paneles Solares y Energía Renovable",
+  description:
+    "Energia Solar Canarias ofrece instalación de paneles solares, energía solar fotovoltaica y soluciones de autoconsumo solar en Tenerife, Gran Canaria, Lanzarote, y más.",
+  authors: [{ name: "Energia Solar Canarias" }],
+  icons: {
+    icon: "/assets/icons/favicon.ico",
+    shortcut: "/assets/icons/favicon.ico",
+    apple: "/assets/icons/apple-touch-icon.png",
+  },
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Energia Solar Canarias ofrece instalación de paneles solares, energía solar fotovoltaica y soluciones de autoconsumo solar en Tenerife, Gran Canaria, Lanzarote, y más."
-        />
-        <meta name="author" content="Energia Solar Canarias" />
-        <title>
-          Energia Solar Canarias - Instalación de Paneles Solares y Energía
-          Renovable
-        </title>
-        <link rel="icon" href="/icon.png" sizes="any" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-48x48.png"
-          sizes="48x48"
-        />
-        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/apple-touch-icon.png"
-        />
-        <meta
-          name="apple-mobile-web-app-title"
-          content="Energia Solar Canarias"
-        />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-
       <body>
         <TranslationProvider>
           <StoreProvider>{children}</StoreProvider>

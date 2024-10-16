f12a155cc73fb0decc972434d04a762d4f88ab24
Thomas Augot <thomas.augot@hotmail.fr>
2024-10-16 15:50:14 +0100

    cambio backdrop blur

--- components/SelectAddressOnMap.js
+++ components/SelectAddressOnMap.js
@@ -99,7 +99,7 @@ const SelectAddressOnMap = ({ isOpen, onClose, onLocationSelect }) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-black/30 backdrop-blur-md"
+            className="fixed inset-0 bg-black/50"
             onClick={onClose}
           />
           <motion.div

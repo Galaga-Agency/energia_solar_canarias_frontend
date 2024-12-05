f6aadc61407e5a30e184ba0960596cec279929a7
Thomas Augot <thomas.augot@hotmail.fr>
2024-12-05 14:38:29 +0000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -120,6 +120,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 export default withPWA(nextConfig);

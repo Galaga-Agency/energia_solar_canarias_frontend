955f3ce3f02d9ca916e16a63a6256e9bc0b371a1
Thomas Augot <thomas.augot@hotmail.fr>
2024-12-05 14:40:42 +0000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import withPWAInit from "next-pwa";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const cacheFirstOptions = {
   cacheableResponse: {
